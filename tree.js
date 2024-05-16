f2384889535fecd1fb1561e2282709e87b071cca
thisStirageIsMine <bezrukovvaler@yandex.ru>
2024-05-17 01:10:35 +0300

    funny

---
+++ tree.js
@@ -0,0 +1,57 @@
+const tree = [
+    {
+        v: 5,
+        c: [
+            {
+                v: 10,
+                c: [
+                    {
+                        v: 11,
+                    }
+                ]
+            },
+            {
+                v: 7,
+                c: [
+                    {
+                        v: 5,
+                        c: [
+                            {
+                                v: 1
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        v: 5,
+        c: [
+            {
+                v: 10
+            },
+            {
+                v: 15
+            }
+        ]
+    }
+]
+
+function getSum(tree) {
+    const q = [...tree];
+    let sum = 0
+
+    while (q.length) {
+        const cur = q.shift()
+        sum += cur.v
+
+        if (cur.c) {
+            q.push(...cur.c)
+        }
+    }
+
+    return sum
+}
+
+console.log(getSum(tree))
